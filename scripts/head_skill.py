cb5f1c2d0e8572018e1a2b650f0e3a3ed9daeee1
Kenji Brameld <kenjibrameld@gmail.com>
2023-09-18 03:31:12 +0000

    try and track oppponent
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- scripts/head_skill.py
+++ scripts/head_skill.py
@@ -7,7 +7,8 @@ from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Image
 from vision_msgs.msg import BoundingBox2D
 from wrestle.image_processing import ImageProcessing
 from nao_lola_command_msgs.msg import JointIndexes, JointPositions
-from math import sin, pi, radians
+from math import sin, pi, radians, atan2
+from geometry_msgs.msg import PointStamped
 
 MAX_YAW = 0.0
 PERIOD = 3.0
@@ -21,15 +22,38 @@ class HeadSkill(Node):
     self.publisher = self.create_publisher(JointPositions, 'effectors/joint_positions', 10)
     self.timer = self.create_timer(0.02, self.timer_callback)
     self.time_start = self.get_clock().now()
+    self.opponent_subscription = self.create_subscription(PointStamped, 'opponent_point',
+                                                          self.opponent_callback, 10)
+    self.time_since_obstacle_detected = None
+    self.opponent_heading = 0
 
   def timer_callback(self):
-    time_since_start = (self.get_clock().now() - self.time_start).nanoseconds / 1000000000.0
-    headyaw = MAX_YAW * sin(2.0 * pi * time_since_start / PERIOD)
+    if self.time_since_obstacle_detected is not None:
+      look_at_opponent = ((self.get_clock().now() - self.time_since_obstacle_detected).nanoseconds / 1e9) < 1.0
+    else:
+      look_at_opponent = False
+    # self.get_logger().info(f'look_at_opponent: {look_at_opponent}')
 
     msg = JointPositions()
-    msg.indexes = [JointIndexes.HEADPITCH, JointIndexes.HEADYAW]
-    msg.positions = [radians(-5), headyaw]
+
+    if look_at_opponent:
+      msg.indexes = [JointIndexes.HEADPITCH, JointIndexes.HEADYAW]
+      msg.positions = [radians(-5), min(max(self.opponent_heading, -0.5), 0.5)]
+    else:
+      time_since_start = (self.get_clock().now() - self.time_start).nanoseconds / 1000000000.0
+      headyaw = MAX_YAW * sin(2.0 * pi * time_since_start / PERIOD)
+      msg.indexes = [JointIndexes.HEADPITCH, JointIndexes.HEADYAW]
+      msg.positions = [radians(-5), headyaw]
     self.publisher.publish(msg)
+
+  def opponent_callback(self, opponent_point):
+    # time_since_start = (self.get_clock().now() - self.time_start).nanoseconds / 1000000000.0
+    # if time_since_start < 4.0:
+    #   return
+
+    self.time_since_obstacle_detected = self.get_clock().now()
+    self.opponent_heading = atan2(opponent_point.point.y, opponent_point.point.x)
+
 
 def main(args=None):
 
