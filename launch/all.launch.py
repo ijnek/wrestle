d92643133c84b3600af3fbc04d0b66fa19a847ea
Kenji Brameld <kenjibrameld@gmail.com>
2023-09-23 03:18:36 +0000

    move more aggressively at start
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- launch/all.launch.py
+++ launch/all.launch.py
@@ -48,6 +48,7 @@ def generate_launch_description():
 
     walk_node = Node(package='walk', executable='walk', remappings=[('imu', 'sensors/imu')],
                      parameters=[{'max_forward': 0.4},
+                                 {'max_left': 0.4},
                                  {'sole_z': -0.29},
                                  {'sole_x': -0.03}])
 
